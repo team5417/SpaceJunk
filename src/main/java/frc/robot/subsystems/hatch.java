369d4e98fc026a4a2ed79e1ec84a59480bd15074
hannahX4108 <hannah.xiao4108@gmail.com>
2019-09-15 10:38:58 -0500

    deleted "am here"

--- src/main/java/frc/robot/subsystems/hatch.java
+++ src/main/java/frc/robot/subsystems/hatch.java
@@ -12,7 +12,6 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 /**
  * Add your docs here.
  */
-// am here
 public class hatch extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
