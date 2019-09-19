71791e6b16f3ba17309a74bc20b25a91bc10e8cc
Eagle Robotics <team5417@gmail.com>
2019-09-19 18:53:57 -0500

    solemoid method and bad intake control

--- src/main/java/frc/robot/subsystems/hatch.java
+++ src/main/java/frc/robot/subsystems/hatch.java
@@ -7,8 +7,11 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
-import frc.robot.constants;
 
 /**
  * Add your docs here.
@@ -17,9 +20,24 @@ public class hatch extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
+  Solenoid solenoid = new Solenoid(1);
+  VictorSPX intake = new VictorSPX(0);
+
   @Override
   public void initDefaultCommand() {
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
+  }
+
+  public void setSolenoid(Boolean button){
+      if(button) {
+        solenoid.set(!solenoid.get());
+      }  
+  }
+
+  public void intakeControl(Boolean button1, Boolean button2){
+    if(button1){
+      intake.set(ControlMode., demand);
+    }
   }
 }
